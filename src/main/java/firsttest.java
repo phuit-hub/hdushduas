235039c64e7d2c27c9138bb53372c8e356113bd9
phuit <2687552904@qq.com>
2021-05-21 20:54:33 +0800

    修改备注信息为：输出HelloWorld

--- src/main/java/firsttest.java
+++ src/main/java/firsttest.java
@@ -1,5 +1,5 @@
 public class firsttest {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");//jsiasjd
+        System.out.println("HelloWorld");//输出HelloWorld
     }
 }
